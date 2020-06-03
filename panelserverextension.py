22cdca8812b7e5a06ebeee9a0b6dc61e78ac13b0
marcs994 <62511824+marcs994@users.noreply.github.com>
2020-06-03 15:07:07 +0200

    Update panelserverextension.py

--- panelserverextension.py
+++ panelserverextension.py
@@ -1,5 +1,5 @@
 from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
-    """serve the clifford.ipynb directory with bokeh server"""
+    """serve the Corina_Tracker.ipynb directory with bokeh server"""
     Popen(["panel", "serve", "Corina_Trackerv2.ipynb", "--allow-websocket-origin=*"])
